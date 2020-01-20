e1cee7d7d0cea40205b79facc7dfabb4988679f9
hemayet-nirjhoy <hemayet.nirjhoy@gmail.com>
2020-01-20 21:50:32 +0600

    Initial version

---
+++ signin.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE signin;
+
+CREATE TABLE user_info(
+    name varchar(50) PRIMARY KEY NOT NULL,
+    password VARCHAR(50)
+);
+
+INSERT INTO user_info VALUES('hemayet.nirjhoy@gmail.com','nothing');
+    
